73ebe97c6ff06731d19c11da95a5941ff554a09b
gs <chekirilla@gmail.com>
2024-08-14 02:42:07 +0700

    feat: white list of names

--- main.go
+++ main.go
@@ -1,10 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
+	white_list := [4]string{
+		"Иван",
+		"Пётр",
+		"Николай",
+		"Сергей",
+	}
+
 	var name string
 	fmt.Print("Введите имя: ")
 	fmt.Scanf("%s", &name)
-	fmt.Printf("Hello %s!\n", name)
+	for _, white_list_name := range white_list {
+		if name == white_list_name {
+			fmt.Printf("Hello %s!\n", name)
+			os.Exit(0)
+		}
+	}
+	fmt.Printf("Name \"%s\" not allowed. Please, change you name.\n", name)
 }
