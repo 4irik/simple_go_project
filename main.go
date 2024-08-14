2488ee564b319d47482e70a7e6ee2808f2f33fa6
gs <chekirilla@gmail.com>
2024-08-14 19:47:10 +0700

    feat: move checking for whitelist to a separate function and testing it

--- main.go
+++ main.go
@@ -2,25 +2,32 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
 
 func main() {
-	white_list := [4]string{
+	whiteList := []string{
 		"Иван",
 		"Пётр",
 		"Николай",
 		"Сергей",
 	}
-
 	var name string
+
 	fmt.Print("Введите имя: ")
 	fmt.Scanf("%s", &name)
-	for _, white_list_name := range white_list {
-		if name == white_list_name {
-			fmt.Printf("Hello %s!\n", name)
-			os.Exit(0)
+	if InWhiteList(name, whiteList) {
+		fmt.Printf("Hello %s!\n", name)
+	} else {
+		fmt.Printf("Name \"%s\" not allowed. Please, change you name.\n", name)
+	}
+}
+
+func InWhiteList(name string, whileList []string) bool {
+	for _, allowedName := range whileList {
+		if name == allowedName {
+			return true
 		}
 	}
-	fmt.Printf("Name \"%s\" not allowed. Please, change you name.\n", name)
+
+	return false
 }
