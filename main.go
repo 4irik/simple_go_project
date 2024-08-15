5134b70df42a43bf2c526dd2b484d42e9516a801
gs <chekirilla@gmail.com>
2024-08-15 20:38:43 +0700

    feat: add command `exit` for exit or infinite loop for asking name

--- main.go
+++ main.go
@@ -11,11 +11,18 @@ func main() {
 	whiteList.Add("Пётр")
 	whiteList.Add("Николай")
 
-	fmt.Print("Введите имя: ")
-	fmt.Scanf("%s", &name)
-	if whiteList.IsExist(name) {
-		fmt.Printf("Hello %s!\n", name)
-	} else {
-		fmt.Printf("Name \"%s\" not allowed. Please, change you name.\n", name)
+loop:
+	for {
+		fmt.Print("Введите имя: ")
+		fmt.Scanf("%s", &name)
+		switch true {
+		case name == "exit":
+			break loop
+		case whiteList.IsExist(name):
+			fmt.Printf("Hello %s!\n", name)
+		default:
+			fmt.Printf("Name \"%s\" not allowed. Please, change you name.\n", name)
+			break loop
+		}
 	}
 }
